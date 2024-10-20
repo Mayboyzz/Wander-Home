4216dd0d614591730b21182cbce50413b3a8f3cc
Mayboyzz <mayboyz99@gmail.com>
2024-10-20 12:54:25 -0500

    Added new spots to seeders

--- backend/db/seeders/20240922004312-demo-spotimage.js
+++ backend/db/seeders/20240922004312-demo-spotimage.js
@@ -8,33 +8,35 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await SpotImage.bulkCreate([
-			{
-				spotId: 1,
-				url: "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg",
-				preview: true,
-			},
-			{
-				spotId: 1,
-				url: "https://colleenmcnally.com/wp-content/uploads/2019/06/living-room-and-kitchen-design.jpg",
-				preview: false,
-			},
-			{
-				spotId: 1,
-				url: "https://thearchitecturedesigns.com/wp-content/uploads/2021/06/Decorate-Your-Home-on-a-Budget-6.jpg",
-				preview: false,
-			},
-			{
-				spotId: 1,
-				url: "https://cdn.decorilla.com/online-decorating/wp-content/uploads/2023/06/Bedroom-interior-design-ideas-for-relaxation.jpg?width=900",
-				preview: false,
-			},
-			{
-				spotId: 1,
-				url: "https://media.architecturaldigest.com/photos/64db9eb3be7a08b03e6cd3a0/16:9/w_2580,c_limit/TBHC_50_Bridge_Park_Dr_0900.jpeg",
-				preview: false,
-			},
-		]);
+		for (let i = 1; i < 5; i++) {
+			await SpotImage.bulkCreate([
+				{
+					spotId: i,
+					url: "https://www.bhg.com/thmb/H9VV9JNnKl-H1faFXnPlQfNprYw=/1799x0/filters:no_upscale():strip_icc()/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.jpg",
+					preview: true,
+				},
+				{
+					spotId: i,
+					url: "https://colleenmcnally.com/wp-content/uploads/2019/06/living-room-and-kitchen-design.jpg",
+					preview: false,
+				},
+				{
+					spotId: i,
+					url: "https://thearchitecturedesigns.com/wp-content/uploads/2021/06/Decorate-Your-Home-on-a-Budget-6.jpg",
+					preview: false,
+				},
+				{
+					spotId: i,
+					url: "https://cdn.decorilla.com/online-decorating/wp-content/uploads/2023/06/Bedroom-interior-design-ideas-for-relaxation.jpg?width=900",
+					preview: false,
+				},
+				{
+					spotId: i,
+					url: "https://media.architecturaldigest.com/photos/64db9eb3be7a08b03e6cd3a0/16:9/w_2580,c_limit/TBHC_50_Bridge_Park_Dr_0900.jpeg",
+					preview: false,
+				},
+			]);
+		}
 	},
 
 	async down(queryInterface, Sequelize) {
